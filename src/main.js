bd7f23201b8ab1fbd896a4a976fc1b1f7b37b1dc
AnQi <3193156071@qq.com>
2025-07-23 14:43:07 +0800

    刷新时清除缓存

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css'
 
+// 清除 localStorage 和 sessionStorage
+window.addEventListener('beforeunload', () => {
+    localStorage.clear();
+    sessionStorage.clear();
+});
+
 const app = createApp(App)
 app.use(Antd)
 app.mount('#app')
